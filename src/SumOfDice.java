3bdda7cbdb4ea8d2fcbbab0de7d3b7b5cde37100
Agostino Cavarero <agostino.cavarero@ig.com>
2015-11-12 17:35:59 +0000

    make it more OO

--- src/SumOfDice.java
+++ src/SumOfDice.java
@@ -1,8 +1,23 @@
 public class SumOfDice {
-	
-	private static long[][] cache;
 
-	private static long sumOfDice(int numDice, int numSides, int sum) {
+	private final int numDice;
+	private final int numSides;
+	private final int sum;
+
+	private final long[][] cache;
+
+	public SumOfDice(int numDice, int numSides, int sum) {
+		this.numDice = numDice;
+		this.numSides = numSides;
+		this.sum = sum;
+		this.cache = new long[numDice][sum];
+	}
+
+	public long compute() {
+		return sumOfDice(numDice, numSides, sum);
+	}
+
+	private long sumOfDice(int numDice, int numSides, int sum) {
 		if (sum < numDice || sum > numSides * numDice) {
 			return 0L;
 		} else if (numDice == 1) {
@@ -24,11 +39,8 @@ public class SumOfDice {
 		int numDice = 4;
 		int numSides = 6;
 		int sum = 21;
-		
-		cache = new long[numDice][sum];
-		
-		long res = sumOfDice(numDice, numSides, sum);
+
+		long res = new SumOfDice(numDice, numSides, sum).compute();
 		System.out.printf("There are %d ways for %d dice with %d sides to sum to %d", res, numDice, numSides, sum);
 	}
 }
-
